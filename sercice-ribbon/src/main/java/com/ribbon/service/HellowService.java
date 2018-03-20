0854f4c77995539edecc7346249e22da19c75aca
mxb <15243625369@163.com>
2018-03-20 17:54:49 +0800

    断路器（Hystrix）

--- sercice-ribbon/src/main/java/com/ribbon/service/HellowService.java
+++ sercice-ribbon/src/main/java/com/ribbon/service/HellowService.java
@@ -1,5 +1,6 @@
 package com.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,15 @@ public class HellowService {
     @Autowired
     private RestTemplate restTemplate;
 
+    //该注解对该方法创建了熔断器的功能，并指定了fallbackMethod熔断方法
+    @HystrixCommand(fallbackMethod = "hError")
     public String hiService(String name){
         return restTemplate.getForObject("http://service-first/first?name="+name,String.class);
     }
+
+    public String hError(String name){
+        return "hellow,"+ name + ",sorry,Error";
+    }
+
+
 }
