a6deb178a5499a07e38fb001f07ec150bf791c88
mxb <15243625369@163.com>
2018-03-23 10:45:28 +0800

    通过rabiitmq设置消息总线，刷新配置文件

--- config-client/src/main/java/com/congifclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/congifclient/ConfigClientApplication.java
@@ -3,10 +3,11 @@ package com.congifclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@RefreshScope
 @SpringBootApplication
 @Controller
 public class ConfigClientApplication {
@@ -17,10 +18,18 @@ public class ConfigClientApplication {
 
 	@Value("${foo}")
 	String foo;
+
+	@Value("${democonfigclient.message}")
+	String message;
+
+	@Value("${name}")
+	String name;
+
+	//   调取 http://localhost:8881/actuator/bus-refresh 实现配置的动态刷新
 	@ResponseBody
 	@RequestMapping(value = "/hi")
 	public String hi(){
-		return foo;
+		return foo+message+name;
 	}
 
 }
